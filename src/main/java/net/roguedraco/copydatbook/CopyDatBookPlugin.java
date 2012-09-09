b236d6a6816b2cf183c43e35204fff144fbe8828
Dan Spiteri <danielspiteri@live.co.uk>
2012-09-09 11:50:06 +0100

    Break :P

--- src/main/java/net/roguedraco/copydatbook/CopyDatBookPlugin.java
+++ src/main/java/net/roguedraco/copydatbook/CopyDatBookPlugin.java
@@ -1,5 +1,134 @@
 package net.roguedraco.copydatbook;
 
-public class CopyDatBookPlugin {
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import net.roguedraco.jumpports.Metrics;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.bukkit.util.CommandsManagerRegistration;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissionsException;
+import com.sk89q.minecraft.util.commands.CommandUsageException;
+import com.sk89q.minecraft.util.commands.CommandsManager;
+import com.sk89q.minecraft.util.commands.MissingNestedCommandException;
+import com.sk89q.minecraft.util.commands.WrappedCommandException;
+
+public class CopyDatBookPlugin extends JavaPlugin {
+
+	private static CopyDatBookPlugin plugin;
+	
+	private static String pluginName;
+	private static String pluginVersion;
+	
+	private static Logger logger = Logger.getLogger("Minecraft");
+	 
+	public static CopyDatBookPlugin getPlugin() {
+		return plugin;
+	}
+
+	private CommandsManager<CommandSender> commands;
+	
+	public void onEnable() {
+		CopyDatBookPlugin.plugin = this;
+		CopyDatBookPlugin.pluginName = this.getDescription().getName();
+		CopyDatBookPlugin.pluginVersion = this.getDescription().getVersion();
+		
+		setupCommands();
+		
+		try {
+		    Metrics metrics = new Metrics(this);
+		    metrics.start();
+		} catch (IOException e) {
+		    // Failed to submit the stats :-(
+		}
+		
+		log(Lang.get("plugin.enabled"));
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	private void setupCommands() {
+		this.commands = new CommandsManager<CommandSender>() {
+			@Override
+			public boolean hasPermission(CommandSender sender, String perm) {
+				return sender.hasPermission(perm);
+			}
+		};
+
+		CommandsManagerRegistration cmdRegister = new CommandsManagerRegistration(
+				this, this.commands);
+		cmdRegister.register(GeneralCommands.class);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args) {
+		try {
+			this.commands.execute(cmd.getName(), args, sender, sender);
+		} catch (CommandPermissionsException e) {
+			sender.sendMessage(ChatColor.RED
+					+ Lang.get("exceptions.noPermission"));
+		} catch (MissingNestedCommandException e) {
+			sender.sendMessage(ChatColor.RED + e.getUsage());
+		} catch (CommandUsageException e) {
+			sender.sendMessage(ChatColor.RED + e.getMessage());
+			sender.sendMessage(ChatColor.RED + e.getUsage());
+		} catch (WrappedCommandException e) {
+			if (e.getCause() instanceof NumberFormatException) {
+				sender.sendMessage(ChatColor.RED
+						+ Lang.get("exceptions.numExpected"));
+			} else {
+				sender.sendMessage(ChatColor.RED
+						+ Lang.get("exceptions.errorOccurred"));
+				e.printStackTrace();
+			}
+		} catch (CommandException e) {
+			sender.sendMessage(ChatColor.RED + e.getMessage());
+		}
+
+		return true;
+	}
+	
+	public static void log(String message) {
+		log(Level.INFO, message);
+	}
+
+	public static void log(Level level, String message) {
+		if (plugin.getConfig().getBoolean("useFancyConsole") == true
+				&& level == Level.INFO) {
+			ConsoleCommandSender console = Bukkit.getServer()
+					.getConsoleSender();
+			console.sendMessage("[" + ChatColor.LIGHT_PURPLE + pluginName
+					+ " v" + pluginVersion + ChatColor.GRAY + "] " + message);
+		} else {
+			CopyDatBookPlugin.logger.log(level, "[" + pluginName + " v"
+					+ pluginVersion + "] " + message);
+		}
+	}
+
+	public static void debug(String message) {
+		if (plugin.getConfig().getBoolean("debug")) {
+			if (plugin.getConfig().getBoolean("useFancyConsole") == true) {
+				ConsoleCommandSender console = Bukkit.getServer()
+						.getConsoleSender();
+				console.sendMessage("[" + ChatColor.LIGHT_PURPLE + pluginName
+						+ " v" + pluginVersion + " Debug" + ChatColor.GRAY
+						+ "] " + message);
+			} else {
+				System.out.println("[" + pluginName + " v" + pluginVersion
+						+ " Debug" + "] " + message);
+			}
+		}
+	}
+	
 }
